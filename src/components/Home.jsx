f218e51107e2dd68646da68f49cd2024cf639a2c
Hanna <hannapototska@gmail.com>
2023-12-08 13:56:19 +0100

    added links from home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Navbar from '../layout/Navbar'
 import Footer from '../layout/Footer'
+import { Link } from 'react-router-dom'
 function Home() {
   return (
     <div>
@@ -11,7 +12,9 @@ function Home() {
       <div className='w-screen h-screen bg-background text-text pt-24 text-center main flex flex-col justify-around gap-12 items-center'>
         <h2 className='font-bold text-xl'>Bring your gaming to the <span className='text-accent'>next</span> level</h2>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad quasi nulla et omnis voluptatibus eos vel, numquam libero iusto facere voluptate recusandae? Dolorem, maxime. Facere accusamus autem odit cum accusantium.</p>
+        <Link to={"/community"}>
         <button className='btn w-80 h-16 text-text text-xl bg-secondary border-none'>LEVEL UP NOW</button>
+        </Link>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt rerum fugiat distinctio. Libero, similique corrupti! Suscipit asperiores praesentium veniam nulla possimus ratione, nesciunt omnis iure beatae officia in porro maiores?</p>
       </div>
       
--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import logo from "../logo-gamehub.png";
+import { Link } from 'react-router-dom';
 
 
 function Navbar() {
@@ -8,13 +9,19 @@ function Navbar() {
       <div className="navbar bg-background text-text flex gap-10">
   <div className="navbar-start flex flex-col">
   <a> <img className='w-12' src={logo} alt="logo" /> </a>
-    <a className="text-secondary text-2xl">GameHub</a>
+   <Link to={"/"}>
+   <a className="text-secondary text-2xl">GameHub</a>
+   </Link> 
   </div>
   <div className="navbar-center">
+  <Link to={"/community"}>
     <a className="btn btn-ghost text-xl">Community</a>
+  </Link>
   </div>
   <div className="navbar-end">
+  <Link to={"/register"}>
     <a className="btn bg-primary text-text border-none text-lg">Login</a>
+  </Link>
   </div>
 </div>
    
