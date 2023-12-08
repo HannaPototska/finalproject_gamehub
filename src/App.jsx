f73befdd7a7d7e36c21e571a074d9a4accb76221
Hanna <hannapototska@gmail.com>
2023-12-08 10:36:54 +0100

    created components

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
+import { Link, Route, Routes } from 'react-router-dom';
 import './App.css';
+
 
 function App() {
   return (
     <div className="App bg-background w-screen h-screen">
+   
+   
    
      
     </div>
