48322d4226a3b3537b65df13843badaf59c295a1
Hanna <hannapototska@gmail.com>
2023-12-10 13:39:17 +0100

    cheking if can get API data using axios

--- src/components/Community.jsx
+++ src/components/Community.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useState } from 'react'
+
 
 function Community() {
+  const [games, setgames] = useState([])
+  function getGames() {
+    axios("https://www.freetogame.com/api/games").then(res => 
+    {console.log(res.data)
+    setgames(i => res.data)
+    })
+  .catch(err => console.log(err))
+  }
   return (
-    <div>Community</div>
+    <div>
+      <button className='btn' onClick={getGames}>games</button>
+      {games && games.map(i => <div>
+        {i.title}
+      </div>)}
+    </div>
   )
 }
 
