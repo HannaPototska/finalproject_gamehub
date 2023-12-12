806a433b599d176645b2600a427041e09cff71f9
Hanna <hannapototska@gmail.com>
2023-12-12 12:38:55 +0100

    created function to save user inputs in table of backendless

--- src/components/CreatePost.jsx
+++ src/components/CreatePost.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
 import NavLogged from '../layout/NavLogged'
 import Footer from '../layout/Footer'
+import Backendless from 'backendless'
+
 
 function CreatePost({currentUser, setcurrentUser,games}) {
+  function postTutorial(e) {
+    e.preventDefault()
+
+    const tutorial = {
+      title: e.target.title.value,
+      content: e.target.content.value,
+      game: e.target.gamenames.value,
+      picture:e.target.imgURL.value
+  }
+  
+  Backendless.Data.of( "tutorials" ).save( tutorial )
+    .then( res => {
+        console.log( "new tutorial has been saved" + res);
+      })
+    .catch(err => {
+        console.log( "an error has occurred " + err.message );
+      });
+  }
+
+
   return (
     <div className='h-screen'>
       <NavLogged currentUser={currentUser} setcurrentUser={setcurrentUser} />
@@ -11,11 +33,14 @@ function CreatePost({currentUser, setcurrentUser,games}) {
 
           <h1 className='text-text text-4xl'>Create New Post</h1>
 
-          <form>
+          <form onSubmit={postTutorial}>
             <input type="text" placeholder='Title:' name="title" />
-           <select name="game-names" id="game-names">
-            <option value="" disabled>Choose games</option>
-            {games && games.map(i =>  <option value={i.title}>{i.title}</option>)}
+            <input type="url" name="imgURL" placeholder='Image URL' id="" />
+            
+            <p className='text-text'>Choose Game:</p>
+           <select className='overflow-auto' name="gamenames" id="gamenames">
+            <option value="" disabled>Games:</option>
+            {games && games.map((i,j) =>  <option key={j} value={i.title}>{i.title}</option>)}
            </select>
             <textarea name="content" placeholder='Text:' id="" cols="30" rows="10"></textarea>
           <button type='submit' className='btn border-none text-text bg-accent'>Post</button>
