74c75c22c0236e73582e60f39e928985043a762c
Hanna <hannapototska@gmail.com>
2023-12-13 08:58:01 +0100

    styling createpost page and added navigation after creating

--- src/components/CreatePost.jsx
+++ src/components/CreatePost.jsx
@@ -2,9 +2,13 @@ import React from 'react'
 import NavLogged from '../layout/NavLogged'
 import Footer from '../layout/Footer'
 import Backendless from 'backendless'
+import { useNavigate } from 'react-router-dom'
 
 
 function CreatePost({currentUser, setcurrentUser,games}) {
+
+  const navigate = useNavigate()
+
   function postTutorial(e) {
     e.preventDefault()
 
@@ -24,26 +28,32 @@ function CreatePost({currentUser, setcurrentUser,games}) {
       });
   }
 
+  function clickHandler() {
+    const interval = setInterval(() => {
+      navigate("/profile")
+}, 2000);
+  }
+
 
   return (
-    <div className='h-screen'>
+    <div className='h-screen '>
       <NavLogged currentUser={currentUser} setcurrentUser={setcurrentUser} />
 
       <main className='h-5/6 bg-background flex flex-col items-center'>
 
-          <h1 className='text-text text-4xl'>Create New Post</h1>
+          <h1 className='text-text text-4xl m-3'>Create New Post</h1>
 
-          <form onSubmit={postTutorial}>
-            <input type="text" placeholder='Title:' name="title" />
-            <input type="url" name="imgURL" placeholder='Image URL' id="" />
+          <form className='flex flex-col items-center gap-6' onSubmit={postTutorial}>
+            <input type="text" required placeholder='Title:' name="title" />
+            <input type="url" required name="imgURL" placeholder='Image URL' id="" />
             
             <p className='text-text'>Choose Game:</p>
-           <select className='overflow-auto' name="gamenames" id="gamenames">
+           <select className='overflow-auto h-12 ' name="gamenames" id="gamenames">
             <option value="" disabled>Games:</option>
             {games && games.map((i,j) =>  <option key={j} value={i.title}>{i.title}</option>)}
            </select>
-            <textarea name="content" placeholder='Text:' id="" cols="30" rows="10"></textarea>
-          <button type='submit' className='btn border-none text-text bg-accent'>Post</button>
+            <textarea className='p-3' required name="content" placeholder='Text:' id="" cols="40" rows="10"></textarea>
+           <button onClick={clickHandler} type='submit' className='btn border-none text-text bg-accent w-52 text-xl'>Post</button>
            
           </form>
 
@@ -51,6 +61,7 @@ function CreatePost({currentUser, setcurrentUser,games}) {
       </main>
 
       <Footer />
+      
     </div>
   )
 }
